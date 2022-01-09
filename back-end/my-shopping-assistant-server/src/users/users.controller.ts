194c3d7037ed60a3ab327a9484bc2bf913002498
NanderSantos <nandermgsdp@gmail.com>
2022-01-08 21:18:40 -0300

    Criaçao do README com os contratos de dados do back.

--- back-end/my-shopping-assistant-server/src/users/users.controller.ts
+++ back-end/my-shopping-assistant-server/src/users/users.controller.ts
@@ -20,7 +20,6 @@ export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Post()
-	@UseGuards(JwtAuthGuard)
 	create(@Body() createUserDto: CreateUserDto) {
 		return this.usersService.create(createUserDto);
 	}
